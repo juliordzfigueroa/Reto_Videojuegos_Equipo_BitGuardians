b072845e8c8c9e7fa327a3ba1dacdc3f2e3a520d
Aquiba Yudah Benarroch Bittan <142257549+aquiba-benarroch@users.noreply.github.com>
2025-03-27 20:14:25 -0600

    Update game_classes.js

--- Videojuegos/VideojuegosJS/Juego/js/game_classes.js
+++ Videojuegos/VideojuegosJS/Juego/js/game_classes.js
@@ -33,8 +33,8 @@ class Vec {
         return new Vec(this.x / this.get_length(), this.y / this.get_length());
     }
 
-    distanceTo(other){ // Método para obtener la distancia entre dos vectores.
-        return Math.sqrt((this.x - other.x)**2 + (this.y - other.y)**2);
+    distanceTo(other) { // Método para obtener la distancia entre dos vectores.
+        return Math.sqrt((this.x - other.x) ** 2 + (this.y - other.y) ** 2);
     }
 }
 
