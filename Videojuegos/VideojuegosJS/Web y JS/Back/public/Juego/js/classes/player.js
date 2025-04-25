8a02368768800ac58c59294b5362f14b4fa81dcc
Aquiba Yudah Benarroch Bittan <a01783710@tec.mx>
2025-04-25 17:58:24 -0600

    Ataque corregido

--- Videojuegos/VideojuegosJS/Web y JS/Back/public/Juego/js/classes/player.js
+++ Videojuegos/VideojuegosJS/Web y JS/Back/public/Juego/js/classes/player.js
@@ -11,13 +11,13 @@ let playerspeed = 0.005; // Atributo de velocidad del jugador
 
 class Player extends AnimatedObject {
     constructor(color, width, height, x, y, type, hp, weapon, max_hp, shield, max_shield) {
-        super("green", width*2, height*2.4, x, y, "player");
+        super("green", width * 2, height * 2.4, x, y, "player");
         this.velocity = new Vec(0.0, 0.0);
         this.max_hp = 100; // Atributo de vida máxima del jugador, la cual podrá ser incrementada con powerups.
         this.hp = this.max_hp; // Atributo de vida del jugador
         this.shield = 0; // Atributo de escudo del jugador
-        this.max_shield = this.max_hp*0.1; // Atributo de escudo máximo del jugador, el cual podrá ser incrementado con powerups, el escudo será del 10% de la vida del jugador
-        this.hitBox = new HitBox(this.position.x, this.position.y, this.size.x*0.7, this.size.y*0.9); // Hitbox del jugador
+        this.max_shield = this.max_hp * 0.1; // Atributo de escudo máximo del jugador, el cual podrá ser incrementado con powerups, el escudo será del 10% de la vida del jugador
+        this.hitBox = new HitBox(this.position.x, this.position.y, this.size.x * 0.7, this.size.y * 0.9); // Hitbox del jugador
         this.totalHP = this.hp + this.shield; // Atributo de vida total del jugador, el cual será la suma de la vida y el escudo del jugador
         this.hasEMP = false; // Atributo de si el jugador tiene un EMP o no
         this.emp = null; // Atributo del powerup EMP del jugador, el será solo usado para imagen del hub
@@ -36,7 +36,7 @@ class Player extends AnimatedObject {
         this.touchedCable = false;
 
         this.weapon = getRandomInitWeapon(); // Arma inicial del jugador, aleatoria al inicio
-      
+
         this.sfx = { // Sonidos del jugador
             damage: new Audio("../assets/sfx/Sound_Effects/cipher_impact.mp3"),
             shoot: new Audio("../assets/sfx/Sound_Effects/laser_gun.mp3"),
@@ -55,7 +55,7 @@ class Player extends AnimatedObject {
         this.jefesDerrotados = 0; // Atributo de jefes derrotados por el jugador
         this.partidasJugadas = 0; // Atributo de partidas jugadas por el jugador
         this.partidasGanadas = 0; // Atributo de partidas ganadas por el jugador
-      
+
         // Movimientos del jugador
         this.movement = {
             down: {
@@ -91,7 +91,7 @@ class Player extends AnimatedObject {
                 sign: -1,
                 repeat: true,
                 duration: 50,
-                moveFrames: [40, 46], 
+                moveFrames: [40, 46],
                 idleFrames: [40, 40]
             }
         };
@@ -113,14 +113,14 @@ class Player extends AnimatedObject {
             this.position = newPosition;
         }
 
-        this.hitBox.position.x = this.position.x+0.3; // Actualizar la posición del hitbox del jugador en x
-        this.hitBox.position.y = this.position.y+0.1; // Actualizar la posición del hitbox del jugador en y
+        this.hitBox.position.x = this.position.x + 0.3; // Actualizar la posición del hitbox del jugador en x
+        this.hitBox.position.y = this.position.y + 0.1; // Actualizar la posición del hitbox del jugador en y
         this.hitBox.update(); // Actualizar el hitbox del jugador
 
-        if (this.hp > this.max_hp){ // Si la vida del jugador supera la vida máxima, se le asigna la vida máxima.
+        if (this.hp > this.max_hp) { // Si la vida del jugador supera la vida máxima, se le asigna la vida máxima.
             this.hp = this.max_hp;
         }
-        if (this.shield > this.max_shield){ // Si el escudo del jugador supera el escudo máximo, se le asigna el escudo máximo.
+        if (this.shield > this.max_shield) { // Si el escudo del jugador supera el escudo máximo, se le asigna el escudo máximo.
             this.shield = this.max_shield;
         }
         this.totalHP = this.hp + this.shield; // Actualizar la vida total del jugador
@@ -143,7 +143,7 @@ class Player extends AnimatedObject {
         }
 
         this.updateFrame(deltaTime);
-      
+
         this.cableDamageTimer += deltaTime; // Aumentar el timer del daño del cable
     }
 
@@ -172,64 +172,64 @@ class Player extends AnimatedObject {
         let attackWidth, attackHeight, attackX, attackY; // Variables para la hitbox del ataque
         // Según la dirección, posiciona la hitbox en frente del jugador
         switch (direction) {
-        case "up":
-            if (this.weapon.wtype === "taser") {
-                attackWidth = this.size.x * 0.6;
-                attackHeight = this.size.y * 0.5;
-                attackX = this.position.x + this.size.x*0.19; 
-                attackY = this.position.y - attackHeight;
-            }
-            else if (this.weapon.wtype === "sword") {
-                attackWidth = this.size.x * 1;
-                attackHeight = this.size.y * 0.4;
-                attackX = this.position.x; 
-                attackY = this.position.y - attackHeight;
-            }
-            break;
-        case "down":
-            if (this.weapon.wtype === "taser") {
-                attackWidth = this.size.x * 0.6;
-                attackHeight = this.size.y * 0.5;
-                attackX = this.position.x + this.size.x*0.19; 
-                attackY = this.position.y + this.size.y;
-            }
-            else if (this.weapon.wtype === "sword") {
-                attackWidth = this.size.x * 1;
-                attackHeight = this.size.y * 0.4;
-                attackX = this.position.x; 
-                attackY = this.position.y + this.size.y;
-            }
-            break;
-        case "left":
-            if (this.weapon.wtype === "taser") {
-                attackWidth = this.size.x * 0.5;
-                attackHeight = this.size.y * 0.6;
-                attackY = this.position.y + this.size.y*0.2;
-                attackX = this.position.x - attackWidth;
-            }
-            else if (this.weapon.wtype === "sword") {
-                attackWidth = this.size.x * 0.4;
-                attackHeight = this.size.y * 1;
-                attackY = this.position.y;
-                attackX = this.position.x - attackWidth;
-            }
-            break;
-        case "right":
-            if (this.weapon.wtype === "taser") {
-                attackWidth = this.size.x * 0.5;
-                attackHeight = this.size.y * 0.6;
-                attackY = this.position.y + this.size.y*0.2;
-                attackX = this.position.x + this.size.x;
-            }
-            else if (this.weapon.wtype === "sword") {
-                attackWidth = this.size.x * 0.4;
-                attackHeight = this.size.y * 1;
-                attackY = this.position.y;
-                attackX = this.position.x + this.size.x;
-            }
-            break;
+            case "up":
+                if (this.weapon.wtype === "taser") {
+                    attackWidth = this.size.x * 0.6;
+                    attackHeight = this.size.y * 0.5;
+                    attackX = this.position.x + this.size.x * 0.19;
+                    attackY = this.position.y - attackHeight;
+                }
+                else if (this.weapon.wtype === "sword") {
+                    attackWidth = this.size.x * 1;
+                    attackHeight = this.size.y * 0.4;
+                    attackX = this.position.x;
+                    attackY = this.position.y - attackHeight;
+                }
+                break;
+            case "down":
+                if (this.weapon.wtype === "taser") {
+                    attackWidth = this.size.x * 0.6;
+                    attackHeight = this.size.y * 0.5;
+                    attackX = this.position.x + this.size.x * 0.19;
+                    attackY = this.position.y + this.size.y;
+                }
+                else if (this.weapon.wtype === "sword") {
+                    attackWidth = this.size.x * 1;
+                    attackHeight = this.size.y * 0.4;
+                    attackX = this.position.x;
+                    attackY = this.position.y + this.size.y;
+                }
+                break;
+            case "left":
+                if (this.weapon.wtype === "taser") {
+                    attackWidth = this.size.x * 0.5;
+                    attackHeight = this.size.y * 0.6;
+                    attackY = this.position.y + this.size.y * 0.2;
+                    attackX = this.position.x - attackWidth;
+                }
+                else if (this.weapon.wtype === "sword") {
+                    attackWidth = this.size.x * 0.4;
+                    attackHeight = this.size.y * 1;
+                    attackY = this.position.y;
+                    attackX = this.position.x - attackWidth;
+                }
+                break;
+            case "right":
+                if (this.weapon.wtype === "taser") {
+                    attackWidth = this.size.x * 0.5;
+                    attackHeight = this.size.y * 0.6;
+                    attackY = this.position.y + this.size.y * 0.2;
+                    attackX = this.position.x + this.size.x;
+                }
+                else if (this.weapon.wtype === "sword") {
+                    attackWidth = this.size.x * 0.4;
+                    attackHeight = this.size.y * 1;
+                    attackY = this.position.y;
+                    attackX = this.position.x + this.size.x;
+                }
+                break;
         }
-        
+
         if (this.weapon.wtype === "sword") {
             this.sfx.blade.currentTime = 0; // reinicia si ya estaba sonando
             this.sfx.blade.play(); // Sonido de ataque con espada
@@ -245,7 +245,7 @@ class Player extends AnimatedObject {
         this.attackTimer = 0;
         this.nextAttack = 1000; // Reinicia el temporizador de ataque
         this.attackHitboxTimer = 300; // Duración de la hitbox de ataque
-        
+
         for (let enemy of game.enemies) {
             if (overlapRectangles(this.currentAttackHitbox, enemy)) {
                 enemy.takeDamage(this.weapon.damage);
@@ -256,7 +256,7 @@ class Player extends AnimatedObject {
             }
         }
     }
-    
+
 
     // Método para iniciar el ataque del jugador con la pistola
     shoot(direction) {
@@ -267,7 +267,7 @@ class Player extends AnimatedObject {
                 break;
             case "down":
                 bdirection = new Vec(0, 1); // Dirección hacia abajo
-                break;  
+                break;
             case "left":
                 bdirection = new Vec(-1, 0); // Dirección hacia la izquierda
                 break;
@@ -281,24 +281,39 @@ class Player extends AnimatedObject {
         game.playerBullets.push(bullet); // Añadir la bala al array de balas del enemigo
     }
 
+    // Método para iniciar un ataque en una dirección específica
     startAttack(direction) {
-        if (this.attackCooldownTimer > 0) return; // Si el jugador está en cooldown, no puede atacar
-        else{
-            if (this.isDefeated) return; // Si el jugador fue derrotado, no se puede atacar
-            const dirData = attackAnimations[this.weapon.wtype][direction + "attack"];
-            if (!dirData || dirData.status) return;
-            dirData.status = true;        
-            this.setAnimation(...dirData.moveFrames, true, dirData.duration); // Se cambia a la animación de ataque
-            // Ejecuta la acción de ataque según el tipo de arma
-            if (this.weapon.wtype === "sword" || this.weapon.wtype === "taser") {
-                this.meleeAttack(direction);
-            } 
-            else if (this.weapon.wtype === "gun") {
-                this.shoot(direction);
-            }
-            this.attackCooldownTimer = this.attackCooldownDuration; // Reinicia el temporizador de ataque
-        } 
+        // Si el temporizador de enfriamiento de ataque está activo, evitar atacar
+        if (this.attackCooldownTimer > 0) return;
+        // Si el jugador está derrotado, evitar atacar
+        if (this.isDefeated) return;
+
+        // Obtener los datos de animación de ataque para el arma actual y la dirección
+        const dirData = attackAnimations[this.weapon.wtype][direction + "attack"];
+        if (!dirData || dirData.status) return;
+
+        // Marcar el ataque como activo y establecer la animación de ataque
+        dirData.status = true;
+        this.setAnimation(...dirData.moveFrames, dirData.repeat, dirData.duration);
+
+        // Realizar el ataque correspondiente según el tipo de arma
+        if (this.weapon.wtype === "sword" || this.weapon.wtype === "taser") {
+            this.meleeAttack(direction);
+        } else if (this.weapon.wtype === "gun") {
+            this.shoot(direction); 
+        }
+
+        // Se establece el temporizador de enfriamiento de ataque para evitar ataques inmediatos consecutivos
+        this.attackCooldownTimer = this.attackCooldownDuration; 
+
+        // Restablecer el estado del ataque y detener la animación de ataque después de su duración
+        setTimeout(() => { 
+            dirData.status = false;
+            this.stopAttack(direction);
+        }, dirData.duration);
     }
+
+
 
     stopAttack(direction) {
         if (this.isDefeated) return; // Si el jugador fue derrotado, no se puede atacar
@@ -310,21 +325,21 @@ class Player extends AnimatedObject {
     }
 
     // Método para que el jugador reciba daño
-    takeDamage(damage){
+    takeDamage(damage) {
         this.sfx.damage.currentTime = 0; // reinicia si ya estaba sonando
         this.sfx.damage.play();
-      
+
         this.danoTotalRecibido += damage; // Aumentar el daño total recibido por el jugador
-      
+
         if (this.shield > 0) // Si el jugador tiene escudo, este recibe el daño.
+        {
+            this.shield -= damage;
+            if (this.shield < 0) // Si el daño supera la cantidad de escudo, el daño restante se le aplica a la vida del jugador.
             {
-                this.shield -= damage;
-                if (this.shield < 0) // Si el daño supera la cantidad de escudo, el daño restante se le aplica a la vida del jugador.
-                {
-                    this.hp += this.shield;
-                    this.shield = 0;
-                }
+                this.hp += this.shield;
+                this.shield = 0;
             }
+        }
         else // Si el jugador no tiene escudo, el daño se le aplica directamente a la vida.
         {
             this.hp -= damage;
@@ -341,7 +356,7 @@ class Player extends AnimatedObject {
         }
     }
 
-    powerupEffect(powerup){ // Método para aplicar el efecto del powerup al jugador
+    powerupEffect(powerup) { // Método para aplicar el efecto del powerup al jugador
         this.sfx.powerup.currentTime = 0; // reinicia si ya estaba sonando
         this.sfx.powerup.play(); // Sonido de recogida de powerup
         if (powerup.type === "weapon") {
@@ -353,7 +368,7 @@ class Player extends AnimatedObject {
             droppedWeapon.position = new Vec(powerup.position.x + 5, powerup.position.y);
             game.level.levelPowerUps.push(droppedWeapon);
             GAME_LEVELS[currentRoom].roomPowerUp = droppedWeapon;
-        } 
+        }
         else if (powerup.type === "empBomb") {
             this.hasEMP = true; // El jugador obtiene una bomba EMP.
             this.emp = powerup; // Se guarda para ser usado como imagen
@@ -371,92 +386,92 @@ class Player extends AnimatedObject {
 // Variable que contiene los diferentes frames de ataque del jugador según el arma que use
 const attackAnimations = {
     sword: {
-        upattack: { 
+        upattack: {
             status: false,
             repeat: false,
-            duration: 100, 
-            moveFrames: [91, 94], 
+            duration: 100,
+            moveFrames: [91, 94],
             idleFrames: [90, 90]
         },
-        downattack: { 
+        downattack: {
             status: false,
             repeat: false,
             duration: 100,
-            moveFrames: [61, 63], 
-            idleFrames: [60, 60] 
+            moveFrames: [61, 63],
+            idleFrames: [60, 60]
         },
-        leftattack: { 
+        leftattack: {
             status: false,
             repeat: false,
             duration: 100,
-            moveFrames: [71, 74], 
-            idleFrames: [70, 70] 
+            moveFrames: [71, 74],
+            idleFrames: [70, 70]
         },
-        rightattack: { 
+        rightattack: {
             status: false,
             repeat: false,
             duration: 100,
-            moveFrames: [81, 84], 
+            moveFrames: [81, 84],
             idleFrames: [80, 80]
         }
     },
     taser: {
-        upattack: { 
+        upattack: {
             status: false,
             repeat: false,
             duration: 100,
-            moveFrames: [103, 105], 
+            moveFrames: [103, 105],
             idleFrames: [103, 103]
         },
-        downattack: { 
+        downattack: {
             status: false,
             repeat: false,
             duration: 100,
-            moveFrames: [100, 102], 
-            idleFrames: [100, 100] 
+            moveFrames: [100, 102],
+            idleFrames: [100, 100]
         },
-        leftattack: { 
+        leftattack: {
             status: false,
             repeat: false,
             duration: 100,
-            moveFrames: [110, 113], 
+            moveFrames: [110, 113],
             idleFrames: [110, 110]
         },
-        rightattack: { 
+        rightattack: {
             status: false,
             repeat: false,
-            duration: 100,  
-            moveFrames: [120, 123], 
+            duration: 100,
+            moveFrames: [120, 123],
             idleFrames: [120, 120]
         }
     },
-    gun:{
-        upattack: { 
+    gun: {
+        upattack: {
             status: false,
             repeat: false,
             duration: 50,
-            moveFrames: [134, 137], 
+            moveFrames: [134, 137],
             idleFrames: [134, 134]
         },
-        downattack: { 
+        downattack: {
             status: false,
             repeat: false,
             duration: 50,
-            moveFrames: [130, 133], 
+            moveFrames: [130, 133],
             idleFrames: [130, 130]
         },
-        leftattack: { 
+        leftattack: {
             status: false,
             repeat: false,
             duration: 50,
-            moveFrames: [140, 143], 
+            moveFrames: [140, 143],
             idleFrames: [140, 140]
         },
-        rightattack: { 
+        rightattack: {
             status: false,
             repeat: false,
             duration: 50,
-            moveFrames: [150, 153], 
+            moveFrames: [150, 153],
             idleFrames: [150, 150]
         }
     }
@@ -464,25 +479,25 @@ const attackAnimations = {
 
 // Variable que contiene los frames de estado del jugador, si este es derrotado, si obtiene un power up o si recibe daño.
 const stateAnimations = {
-    damged:{ 
+    damged: {
         status: false,
         repeat: false,
         duration: 50,
-        moveFrames: [55, 58], 
+        moveFrames: [55, 58],
         idleFrames: [0, 4]
     },
     powerup: {
         status: false,
         repeat: false,
         duration: 50,
-        moveFrames: [51, 54], 
+        moveFrames: [51, 54],
         idleFrames: [0, 4]
     },
-    defeated:{ 
+    defeated: {
         status: false,
         repeat: false,
         duration: 800,
-        moveFrames: [163, 167], 
+        moveFrames: [163, 167],
         idleFrames: [0, 4]
     }
 };
